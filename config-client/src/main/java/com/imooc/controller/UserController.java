17159ab2f587b238d58b7456f742c711cd595b2b
One-Meter-Sunlight <chenkaisupervip@163.com>
2018-07-18 09:04:57 +0800

    add code

---
+++ config-client/src/main/java/com/imooc/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.imooc.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 消费者-用户Contorller
+ *
+ * @author ck
+ * @version 1.0
+ * @since 2018-07-10
+ */
+@RestController
+public class UserController {
+
+}
